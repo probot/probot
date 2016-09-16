83e9ee726ee3d8701c5cbb168f424a4ad8472213
Brandon Keepers <bkeepers@github.com>
2016-09-16 09:38:36 -0700

    Respond to new issue

--- server.js
+++ server.js
@@ -1,14 +1,33 @@
-var http = require('http')
-var createHandler = require('github-webhook-handler')
-var handler = createHandler({ path: '/', secret: 'secret' })
+let process = require('process');
+let http = require('http');
+let createHandler = require('github-webhook-handler');
+let webhook = createHandler({ path: '/', secret: 'secret' });
+let GitHubApi = require("github");
+let github = new GitHubApi();
+
+github.authenticate({
+  type: "oauth",
+  token: process.env['GITHUB_TOKEN']
+});
 
 http.createServer(function (req, res) {
-  handler(req, res, function (err) {
+  webhook(req, res, function (err) {
     res.statusCode = 404
     res.end('no such location')
   })
-}).listen(3000)
+}).listen(3000);
 
-handler.on('pull_request', function (event) {
-  console.log('event', event);
-})
+webhook.on('issues', function (event) {
+  var payload = event["payload"];
+
+  github.issues.createComment({
+    user: payload["repository"]["owner"]["login"],
+    repo: payload["repository"]["name"],
+    number: payload["issue"]["number"],
+    body: "Hello world!"
+  }, function(err, res) {
+    if(err) {
+      console.log("ERROR", err, res);
+    }
+  });
+});

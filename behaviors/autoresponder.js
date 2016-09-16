4c96ceb140c8d547643014b6f279b9ecb594b522
Brandon Keepers <bkeepers@github.com>
2016-09-16 11:45:35 -0700

    Fix lint warnings

--- behaviors/autoresponder.js
+++ behaviors/autoresponder.js
@@ -9,33 +9,38 @@
 //     review this as soon as we can.
 //
 
+module.exports = {webhook: 'issues', action: autorespond};
+
 let handlebars = require('handlebars');
 
-module.exports = function(webhook, github) {
-  webhook.on('issues', function (event) {
-    var payload = event['payload'],
-        user = payload['repository']["owner"]["login"],
-        repo = payload["repository"]["name"];
+function autorespond(event, github) {
+  var payload = event.payload;
+  var user = payload.repository.owner.login;
+  var repo = payload.repository.name;
 
-    // Get template from the repo
-    github.repos.getContent({
+  // Get template from the repo
+  github.repos.getContent({
+    user: user,
+    repo: repo,
+    path: '.github/ISSUE_REPLY_TEMPLATE.md'
+  }, function (err, data) {
+    if (err) {
+      console.log('ERROR', err);
+      return;
+    }
+
+    var template = handlebars.compile(new Buffer(data.content, 'base64').toString());
+
+    // Post issue comment
+    github.issues.createComment({
       user: user,
       repo: repo,
-      path: '.github/ISSUE_REPLY_TEMPLATE.md'
-    }, function(err, data) {
-      var template = handlebars.compile(new Buffer(data['content'], 'base64').toString());
-
-      // Post issue comment
-      github.issues.createComment({
-        user: user,
-        repo: repo,
-        number: payload['issue']['number'],
-        body: template(payload)
-      }, function(err, res) {
-        if(err) {
-          console.log('ERROR', err, res);
-        }
-      });
+      number: payload.issue.number,
+      body: template(payload)
+    }, function (err, res) {
+      if (err) {
+        console.log('ERROR', err, res);
+      }
     });
   });
 }
--- server.js
+++ server.js
@@ -1,21 +1,33 @@
 let process = require('process');
 let http = require('http');
 let createHandler = require('github-webhook-handler');
-let webhook = createHandler({ path: '/', secret: 'secret' });
 let GitHubApi = require('github');
+
+let webhook = createHandler({path: '/', secret: 'secret'});
 let github = new GitHubApi();
 
 github.authenticate({
   type: 'oauth',
-  token: process.env['GITHUB_TOKEN']
+  token: process.env.GITHUB_TOKEN
 });
 
 http.createServer(function (req, res) {
   webhook(req, res, function (err) {
-    res.statusCode = 404
-    res.end('no such location')
-  })
+    if (err) {
+      console.log('ERROR', err);
+      res.statusCode = 500;
+      res.end('Something has gone terribly wrong.');
+    } else {
+      res.statusCode = 404;
+      res.end('no such location');
+    }
+  });
 }).listen(3000);
 
-let autoresponder = require('./behaviors/autoresponder.js');
-autoresponder(webhook, github);
+function register(behavior) {
+  webhook.on(behavior.webhook, function (event) {
+    behavior.action(event, github);
+  });
+}
+
+register(require('./behaviors/autoresponder.js'));

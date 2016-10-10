98a261c65d3f3ba32de7e9122f79edcffb29a5a7
Brandon Keepers <bkeepers@github.com>
2016-10-10 01:46:07 -0500

    Add support for event.action syntax

--- test/dispatcher.js
+++ test/dispatcher.js
@@ -5,17 +5,27 @@ const payload = require ('./fixtures/webhook/comment.created.json');
 
 const createSpy = expect.createSpy;
 
-var github = {
+const github = {
   issues: {
     createComment: createSpy().andReturn(Promise.resolve())
   }
 };
+const event = {event: 'issues', payload: payload};
+const dispatcher = new Dispatcher(github, event);
 
 describe('dispatch', function () {
   describe('reply to new issue with a comment', function() {
-    const event = {event: 'issues', payload: payload};
-    const dispatcher = new Dispatcher(github, event);
     const config = {behaviors: [{on: 'issues', then: {comment: 'Hello World!'}}]};
+
+    it('posts a coment', function() {
+      return dispatcher.call(config).then(function() {
+        expect(github.issues.createComment).toHaveBeenCalled();
+      });
+    });
+  });
+
+  describe('reply to new issue with a comment', function() {
+    const config = {behaviors: [{on: 'issues.created', then: {comment: 'Hello World!'}}]};
 
     it('posts a coment', function() {
       return dispatcher.call(config).then(function() {

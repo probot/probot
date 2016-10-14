719bc875ece0f63902319097a6576dfb5375fef5
Seema Kamath <seema.kamath@gmail.com>
2016-10-14 09:48:02 -0700

    Make debug global

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const Dispatcher = require('./lib/dispatcher');
 const PORT = process.env.PORT || 3000;
 const webhook = createHandler({path: '/', secret: process.env.WEBHOOK_SECRET || 'development'});
 
-var debug = require('debug')('http')
+GLOBAL.debug = require('debug')('PRobot')
   , name = 'PRobot';
 
 debug('Starting %s', name);

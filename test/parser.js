f5ff1bca79237a561e987d45bba691f2b82cc268
Brandon Keepers <bkeepers@github.com>
2016-10-17 00:59:38 -0500

    Implement simple actions

--- test/parser.js
+++ test/parser.js
@@ -6,25 +6,39 @@ var parser = peg.generate(fs.readFileSync('./lib/parser.pegjs').toString());
 describe('parser', () => {
   describe('on', () => {
     it('parses an event', () => {
-      expect(parser.parse('on issues')).toEqual({
-        on: [{name: 'issues'}]
+      expect(parser.parse('on issues then close')).toEqual({
+        on: [{name: 'issues'}],
+        then: [{name: 'close'}]
       });
     });
 
     it('parses an event and action', () => {
-      expect(parser.parse('on issues.opened')).toEqual({
-        on: [{name: 'issues', action: 'opened'}]
+      expect(parser.parse('on issues.opened then close')).toEqual({
+        on: [{name: 'issues', action: 'opened'}],
+        then: [{name: 'close'}]
       });
     });
 
     it('parses multiple events', () => {
-      expect(parser.parse('on issues.opened, pull_request.opened')).toEqual({
+      expect(parser.parse('on issues.opened, pull_request.opened then close')).toEqual({
         on: [
           {name: 'issues', action: 'opened'},
           {name: 'pull_request', action: 'opened'}
+        ],
+        then: [{name: 'close'}]
+      });
+    });
+  });
+
+  describe('then', () => {
+    it('parses multiple words', () => {
+      expect(parser.parse('on issues then close and lock')).toEqual({
+        on: [{name: 'issues'}],
+        then: [
+          {name: 'close'},
+          {name: 'lock'}
         ]
       });
     });
-
   });
 });

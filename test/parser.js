6b5e3d74e3c0c054fed802c0d98a0b0b25307232
Brandon Keepers <bkeepers@github.com>
2016-10-17 01:21:31 -0500

    Implement 'then'

--- test/parser.js
+++ test/parser.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 const fs = require('fs');
-var peg = require("pegjs");
+var peg = require('pegjs');
 var parser = peg.generate(fs.readFileSync('./lib/parser.pegjs').toString());
 
 describe('parser', () => {
@@ -37,6 +37,33 @@ describe('parser', () => {
         then: [
           {name: 'close'},
           {name: 'lock'}
+        ]
+      });
+    });
+
+    it('parses string arguments', () => {
+      expect(parser.parse('on issues then comment("Hello World!")')).toEqual({
+        on: [{name: 'issues'}],
+        then: [
+          {name: 'comment', value: 'Hello World!'}
+        ]
+      });
+    });
+
+    it('parses word arguments', () => {
+      expect(parser.parse('on issues then assign(bkeepers)')).toEqual({
+        on: [{name: 'issues'}],
+        then: [
+          {name: 'assign', value: 'bkeepers'}
+        ]
+      });
+    });
+
+    it('parses multiple word arguments', () => {
+      expect(parser.parse('on issues then assign(bkeepers, hubot)')).toEqual({
+        on: [{name: 'issues'}],
+        then: [
+          {name: 'assign', value: ['bkeepers', 'hubot']}
         ]
       });
     });

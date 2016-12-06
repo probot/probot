8be66d9bc426e564824d3ef32b79f58e45b3163c
Brandon Keepers <bkeepers@github.com>
2016-12-05 22:18:52 -0600

    Switch to bunyan for logging

--- server.js
+++ server.js
@@ -3,18 +3,16 @@ require('dotenv').config({silent: true});
 const process = require('process');
 const http = require('http');
 const createHandler = require('github-webhook-handler');
-const debug = require('debug')('PRobot');
+const log = require('./lib/log');
 const robot = require('./lib/robot');
 
 const PORT = process.env.PORT || 3000;
 const webhook = createHandler({path: '/', secret: process.env.WEBHOOK_SECRET || 'development'});
 
-debug('Starting');
-
 http.createServer((req, res) => {
   webhook(req, res, err => {
     if (err) {
-      console.error(err);
+      log.error(err);
       res.statusCode = 500;
       res.end('Something has gone terribly wrong.');
     } else {
@@ -28,7 +26,7 @@ robot.listen(webhook);
 
 // Show trace for any unhandled rejections
 process.on('unhandledRejection', reason => {
-  console.error(reason);
+  log.error(reason);
 });
 
 console.log('Listening on http://localhost:' + PORT);

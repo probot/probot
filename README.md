4ed502951de2eae879a1d1c597afb15c40628a04
Johannes Nicolai <jonico@github.com>
2016-12-07 18:53:44 +0100

    Added two further examples and kept the original example in README.md

--- README.md
+++ README.md
@@ -13,6 +13,14 @@ _**Heads up!** The [demo integration](https://github.com/integration/probot-demo
 0. Go to the **[demo integration](https://github.com/integration/probot-demo)**, click **Install**, and then select an organization.
 0. Create a `.probot.js` file in your repository with the following contents. See [Configuration](docs/configuration.md) for more information on what behaviors can be built.
 
+        on('issues.opened').comment(`
+          Hello @{{ sender.login }}. Thanks for inviting me to your project.
+          Read more about [all the things I can help you with][config]. I can't
+          wait to get started!
+
+          [config]: https://github.com/bkeepers/PRobot/blob/master/docs/configuration.md
+        `);
+
         include("bkeepers/probot:docs/demo.js");
 
 0. Open a new issue. @probot should post a comment (you may need to refresh to see it).
--- docs/demo.js
+++ docs/demo.js
@@ -2,6 +2,25 @@ on('issues.opened').comment(`
   Hello @{{ sender.login }}. Thanks for inviting me to your project.
   Read more about [all the things I can help you with][config]. I can't
   wait to get started!
-
   [config]: https://github.com/bkeepers/PRobot/blob/master/docs/configuration.md
+`);
+
+// Delete :+1: comments
+const singleEmoji = /^\W*(:[\w-\+]+:|[\uE000-\uF8FF]|\uD83C[\uDF00-\uDFFF]|\uD83D[\uDC00-\uDDFF])\W*$/g;
+on('issue_comment.created')
+  .filter(event => event.payload.comment.body.match(singleEmoji))
+  .deleteComment();
+
+function isDeleted(event) {
+  return event.payload.action === 'deleted' &&
+  event.payload.sender.login !== 'probot-demo[bot]';
+}
+
+// restore deleted comments but the one deleted by PRobot
+on('issue_comment', 'commit_comment', 'pull_request_review_comment')
+.filter(isDeleted)
+.comment(`
+Hi @{{ sender.login }}. Please do not delete comments.
+Deleted comment from {{ comment.user.login }} at {{ comment.updated_at }} was
+{{ comment.body }}
 `);

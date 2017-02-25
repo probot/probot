015a7f05805ade1016f8966e44eb4511908489e4
Brandon Keepers <bkeepers@github.com>
2017-02-24 20:28:50 -0600

    Move server implementation to separate file

--- server.js
+++ server.js
@@ -1,32 +1,24 @@
 require('dotenv').config({silent: true});
 
 const process = require('process');
-const http = require('http');
-const createHandler = require('github-webhook-handler');
+const createWebhook = require('github-webhook-handler');
+
 const log = require('./lib/log');
 const robot = require('./lib/robot');
+const createServer = require('./lib/server');
 
+const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET || 'development';
 const PORT = process.env.PORT || 3000;
-const webhook = createHandler({path: '/', secret: process.env.WEBHOOK_SECRET || 'development'});
 
-http.createServer((req, res) => {
-  webhook(req, res, err => {
-    if (err) {
-      log.error(err);
-      res.statusCode = 500;
-      res.end('Something has gone terribly wrong.');
-    } else {
-      res.statusCode = 404;
-      res.end('no such location');
-    }
-  });
-}).listen(PORT);
-
-robot.listen(webhook);
+const webhook = createWebhook({path: '/', secret: WEBHOOK_SECRET});
+const server = createServer(webhook);
 
 // Show trace for any unhandled rejections
 process.on('unhandledRejection', reason => {
   log.error(reason);
 });
+
+robot.listen(webhook);
+server.listen(PORT);
 
 console.log('Listening on http://localhost:' + PORT);

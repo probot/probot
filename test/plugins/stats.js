a64d573793fe2ec8fdce9f47c5c8253ec9083962
Brandon Keepers <bkeepers@github.com>
2017-09-26 08:29:41 -0500

    Add test with nock

---
+++ test/plugins/stats.js
@@ -0,0 +1,54 @@
+const request = require('supertest')
+const express = require('express')
+const {createRobot} = require('../..')
+const plugin = require('../../lib/plugins/stats')
+const nock = require('nock');
+
+nock.disableNetConnect();
+nock.enableNetConnect(/127\.0\.0\.1/);
+
+describe('stats', function () {
+  let robot
+  let server, github
+
+  beforeEach(async () => {
+    nock('https://api.github.com')
+     .defaultReplyHeaders({'Content-Type': 'application/json'})
+     .get('/app/installations?per_page=100').reply(200, [{id: 1, account: {login: 'testing'}}])
+     .get('/installation/repositories').reply(200, {repositories: [
+       {private: true, stargazers_count: 1},
+       {private: false, stargazers_count: 2}
+     ]})
+
+    const cacheManager = require('cache-manager')
+    const cache = cacheManager.caching({store: 'memory'})
+
+    const GitHubApi = require('github')
+    const app = {
+      async asApp() {
+        return new GitHubApi()
+      },
+
+      async asInstallation() {
+        return new GitHubApi()
+      },
+
+      async createToken() {
+        return {data: {token: 'test'}}
+      }
+    }
+    robot = createRobot({app, cache})
+
+    await plugin(robot)
+
+    server = express()
+    server.use(robot.router)
+  })
+
+  describe('GET /stats', () => {
+    it('returns installation count and popular accounts', () => {
+      return request(server).get('/stats')
+        .expect(200, {'installations': 1, 'popular': [{login: 'testing', stars: 2}]})
+    })
+  })
+})

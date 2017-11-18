8ea45b47b119484fad9b7133bf674106fb8eadbf
JasonEtco <jasonetco@gmail.com>
2017-11-18 00:10:30 -0500

    Add some tests

--- test/plugins/default.test.js
+++ test/plugins/default.test.js
@@ -2,6 +2,8 @@ const request = require('supertest')
 const express = require('express')
 const plugin = require('../../lib/plugins/default')
 const helper = require('./helper')
+const fs = require('fs')
+const path = require('path')
 
 describe('default plugin', function () {
   let server, robot
@@ -19,6 +21,28 @@ describe('default plugin', function () {
   describe('GET /probot', () => {
     it('returns a 200 response', () => {
       return request(server).get('/probot').expect(200)
+    })
+
+    describe('Package data collection', () => {
+      let cwd = process.cwd()
+      const pathTo = file => path.join(__dirname, '..', 'fixtures', 'plugin', file)
+
+      it('returns the correct HTML with values', async () => {
+        const expected = fs.readFileSync(pathTo('with-package.html'), 'utf8')
+        const actual = await request(server).get('/probot').expect(200)
+        expect(actual.text).toBe(expected)
+      })
+
+      it('returns the correct HTML without values', async () => {
+        process.chdir(__dirname)
+        const expected = fs.readFileSync(pathTo('without-package.html'), 'utf8')
+        const actual = await request(server).get('/probot').expect(200)
+        expect(actual.text).toBe(expected)
+      })
+
+      afterAll(() => {
+        process.chdir(cwd)
+      })
     })
   })
 

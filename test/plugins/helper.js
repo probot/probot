53a92a55bbff2fa855dd89370374d52d3a70d1b4
Brandon Keepers <bkeepers@github.com>
2017-11-26 23:16:35 -0600

    Fix lint errors

--- test/plugins/helper.js
+++ test/plugins/helper.js
@@ -1,7 +1,6 @@
 // FIXME: move this to a test helper that can be used by other apps
 
 const cacheManager = require('cache-manager')
-const GitHubApi = require('github')
 const {createRobot} = require('../..')
 
 const cache = cacheManager.caching({store: 'memory'})

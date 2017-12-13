7bef6a7bf97212acff5ce056cfc63b0e919d28a9
Jason Etcovitch <jasonetco@github.com>
2017-12-13 11:13:46 -0500

    chore: Add serializer tests (#369)

--- test/serializers.test.js
+++ test/serializers.test.js
@@ -1,6 +1,25 @@
 const serializers = require('../lib/serializers')
 
 describe('serializers', () => {
+  describe('repository', () => {
+    it('returns the repository\'s full name', () => {
+      const repo = { full_name: 'probot/JasonEtco' }
+      expect(serializers.repository(repo)).toBe('probot/JasonEtco')
+    })
+  })
+
+  describe('installation', () => {
+    it('returns the installation\'s account login', () => {
+      const inst = { account: { login: 'JasonEtco' } }
+      expect(serializers.installation(inst)).toBe('JasonEtco')
+    })
+
+    it('returns the installation if no account exists', () => {
+      const inst = { foo: true, bar: false }
+      expect(serializers.installation(inst)).toEqual(inst)
+    })
+  })
+
   describe('event', () => {
     it('works with a legit event', () => {
       const event = {id: 1,
@@ -37,6 +56,17 @@ describe('serializers', () => {
 
     it('works empty object', () => {
       expect(serializers.event({})).toEqual({})
+    })
+  })
+
+  describe('res', () => {
+    it('returns the provided object if no status exists', () => {
+      const res = { foo: true, bar: false }
+      expect(serializers.res(res)).toEqual(res)
+    })
+
+    it('returns nothing when passed nothing', () => {
+      expect(serializers.res()).toBe(undefined)
     })
   })
 })

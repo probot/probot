a5a276583191424a7714f3f96fd9eaf4379cd4f4
Brandon Keepers <bkeepers@github.com>
2018-01-15 13:51:19 -0600

    Move webhook setup out of server setup
    Registering the webhook inside the `createServer` method adds an unneccessary dependency on the server setup. It is just as easy to call `server.use(webhook)` outside of the setup.

--- test/server.test.js
+++ test/server.test.js
@@ -5,11 +5,9 @@ const logger = createLogger()
 
 describe('server', function () {
   let server
-  let webhook
 
   beforeEach(() => {
-    webhook = jest.fn((req, res, next) => next())
-    server = createServer({webhook, logger})
+    server = createServer({logger})
 
     // Error handler to avoid printing logs
     server.use(function (err, req, res, next) {
@@ -20,13 +18,6 @@ describe('server', function () {
   describe('GET /ping', () => {
     it('returns a 200 response', () => {
       return request(server).get('/ping').expect(200, 'PONG')
-    })
-  })
-
-  describe('webhook handler', () => {
-    it('should 500 on a webhook error', () => {
-      webhook.mockImplementation((req, res, callback) => callback(new Error('webhook error')))
-      return request(server).post('/').expect(500)
     })
   })
 

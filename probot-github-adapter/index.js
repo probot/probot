43a6371eb96dfc3c4464c32ce3731458eaa0201b
Brandon Keepers <bkeepers@github.com>
2018-01-26 19:24:40 -0600

    Extract github auth code to adapter

--- probot-github-adapter/index.js
+++ probot-github-adapter/index.js
@@ -1,22 +1,92 @@
+const createApp = require('../lib/github-app')
+const cacheManager = require('cache-manager')
 const createWebhook = require('github-webhook-handler')
+const Context = require('../lib/context')
+const GitHubApi = require('../lib/github')
 
 module.exports = class GitHubAdapter {
   constructor ({logger}, options) {
     const path = options.webhookPath || '/'
     const secret = options.secret || 'development'
 
+    this.cache = cacheManager.caching({ store: 'memory', ttl: 60 * 60 })
     this.logger = logger
     this.middleware = createWebhook({path, secret})
 
+    this.app = createApp({
+      id: options.id,
+      cert: options.cert
+    })
+
     // Log all webhook errors
-    this.middleware.on('error', logger.error.bind(logger))
+    this.middleware.on('error', err => {
+      logger.error({err})
+    })
   }
 
-  listen (callback) {
-    // Log all received webhooks
-    this.middleware.on('*', event => {
+  listen (handler) {
+    this.middleware.on('*', async event => {
+      // Log all received webhooks
       this.logger.info({event}, 'Webhook received')
-      callback(event)
+
+      // Deliver the event
+      handler(await this.createContext(event))
     })
+  }
+
+  /**
+   * Authenticate and get a GitHub client that can be used to make API calls.
+   *
+   * You'll probably want to use `context.github` instead.
+   *
+   * **Note**: `robot.auth` is asynchronous, so it needs to be prefixed with a
+   * [`await`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/await)
+   * to wait for the magic to happen.
+   *
+   * @example
+   *
+   *  module.exports = function(robot) {
+   *    robot.on('issues.opened', async context => {
+   *      const github = await robot.auth();
+   *    });
+   *  };
+   *
+   * @param {number} [id] - ID of the installation, which can be extracted from
+   * `context.payload.installation.id`. If called without this parameter, the
+   * client wil authenticate [as the app](https://developer.github.com/apps/building-integrations/setting-up-and-registering-github-apps/about-authentication-options-for-github-apps/#authenticating-as-a-github-app)
+   * instead of as a specific installation, which means it can only be used for
+   * [app APIs](https://developer.github.com/v3/apps/).
+   *
+   * @returns {Promise<github>} - An authenticated GitHub API client
+   * @private
+   */
+  async auth (id, log = this.logger.wrap()) {
+    const github = new GitHubApi({
+      debug: process.env.LOG_LEVEL === 'trace',
+      host: process.env.GHE_HOST || 'api.github.com',
+      pathPrefix: process.env.GHE_HOST ? '/api/v3' : '',
+      logger: log.child({installation: id})
+    })
+
+    if (id) {
+      const res = await this.cache.wrap(`app:${id}:token`, () => {
+        log.trace(`creating token for installation`)
+        github.authenticate({type: 'integration', token: this.app()})
+
+        return github.apps.createInstallationToken({installation_id: id})
+      }, {ttl: 60 * 59}) // Cache for 1 minute less than GitHub expiry
+
+      github.authenticate({type: 'token', token: res.data.token})
+    } else {
+      github.authenticate({type: 'integration', token: this.app()})
+    }
+
+    return github
+  }
+
+  async createContext (event) {
+    const logger = this.logger.wrap().child({id: event.id})
+    const client = await this.auth(event.payload.installation.id, logger)
+    return new Context(event, client, logger)
   }
 }
--- probot-github-adapter/index.test.js
+++ probot-github-adapter/index.test.js
@@ -1,3 +1,4 @@
+process.env.LOG_LEVEL = process.env.LOG_LEVEL || 'fatal'
 const request = require('supertest')
 const nock = require('nock')
 
@@ -7,68 +8,133 @@ const payload = require('../test/fixtures/webhook/push')
 const fixture = JSON.stringify(payload, null, 2)
 const cert = require('fs').readFileSync('../test/fixtures/private-key.pem')
 
-nock.disableNetConnect()
 nock.enableNetConnect(/127\.0\.0\.1/)
 
 describe('GitHubAdapter', () => {
-  let probot, options, adapter
+  let probot, options, adapter, api
 
   beforeEach(() => {
+    nock.cleanAll()
+
+    api = nock('https://api.github.com/')
+
     options = {
       id: 1,
-      cert
+      cert,
+      webhookPath: '/',
+      catchErrors: false
     }
 
     // FIXME: refactor tests to load adapter directly
     probot = createProbot(options)
     adapter = probot.adapter
-
-    // Error handler to avoid printing logs
-    // eslint-disable-next-line handle-callback-err
-    probot.server.use((err, req, res, next) => { })
   })
 
-  describe('webhook', () => {
-    it('receives a webhook with POST /', () => {
-      return request(probot.server).post('/')
-        .send(fixture)
-        .set('Content-Type', 'application/json')
-        .set('X-GitHub-Delivery', '1')
-        .set('X-GitHub-Event', 'push')
-        .set('X-Hub-Signature', adapter.middleware.sign(fixture))
-        .expect(200)
+  afterEach(() => {
+    expect(api.pendingMocks()).toEqual([])
+  })
+
+  test('delivers webhooks to the app', async (done) => {
+    // Mock out GitHub API calls that will be performed
+    api.post('/installations/1729/access_tokens')
+      .reply(201, {token: 'test'})
+    api.get('/repos/bkeepers-inc/test/issues/1')
+      .matchHeader('authorization', 'token test')
+      .reply(200, {foo: 'bar'})
+
+    // Load a probot app for testing
+    probot.load(robot => {
+      robot.on('push', async context => {
+        expect(context.event).toEqual('push')
+        const res = await context.github.issues.get(context.repo({number: 1}))
+        expect(res.data).toEqual({foo: 'bar'})
+
+        done()
+      })
     })
 
-    it('fails with an invalid signature', () => {
-      return request(probot.server).post('/')
-        .send(fixture)
-        .set('Content-Type', 'application/json')
-        .set('X-GitHub-Delivery', '2')
-        .set('X-GitHub-Event', 'push')
-        .set('X-Hub-Signature', 'invalid')
-        .expect(400)
+    await request(probot.server).post('/')
+      .send(fixture)
+      .set('Content-Type', 'application/json')
+      .set('X-GitHub-Delivery', '1')
+      .set('X-GitHub-Event', 'push')
+      .set('X-Hub-Signature', adapter.middleware.sign(fixture))
+      .expect(200)
+  })
+
+  test('fails with an invalid signature', () => {
+    return request(probot.server).post('/')
+      .send(fixture)
+      .set('Content-Type', 'application/json')
+      .set('X-GitHub-Delivery', '2')
+      .set('X-GitHub-Event', 'push')
+      .set('X-Hub-Signature', 'invalid')
+      .expect(400)
+  })
+
+  test('does not override GET /', () => {
+    probot.server.get('/', (req, res) => res.end('get-webhook'))
+    return request(probot.server).get('/').expect(200, 'get-webhook')
+  })
+
+  test('allows users to configure webhook path', async (done) => {
+    options.webhookPath = '/webhook'
+    probot = createProbot(options)
+
+    probot.load(robot => {
+      robot.on('push', () => {
+        // test is done when robot receives the event
+        done()
+      })
     })
 
-    it('does not override GET /', () => {
-      probot.server.get('/', (req, res) => res.end('get-webhook'))
-      return request(probot.server).get('/').expect(200, 'get-webhook')
-    })
+    api.post('/installations/1729/access_tokens')
+      .reply(201, {token: 'test'})
 
-    describe('with a custom webhook path', () => {
+    // Post should return webhook response
+    return request(probot.server).post('/webhook')
+      .send(fixture)
+      .set('Content-Type', 'application/json')
+      .set('X-GitHub-Delivery', 'b51f38f0-9fec-11e7-8359-02048d903941')
+      .set('X-GitHub-Event', 'push')
+      .set('X-Hub-Signature', adapter.middleware.sign(fixture))
+      .expect(200, {'ok': true})
+  })
+
+  describe('auth', () => {
+    describe('with an installation id', () => {
       beforeEach(() => {
-        options.webhookPath = '/webhook'
-        probot = createProbot(options)
+        api.post('/installations/1/access_tokens')
+          .reply(201, {token: 'test'})
       })
 
-      it('allows users to configure webhook paths', async () => {
-        // Post should return webhook response
-        await request(probot.server).post('/webhook')
-          .send(fixture)
-          .set('Content-Type', 'application/json')
-          .set('X-GitHub-Delivery', 'b51f38f0-9fec-11e7-8359-02048d903941')
-          .set('X-GitHub-Event', 'push')
-          .set('X-Hub-Signature', adapter.middleware.sign(fixture))
-          .expect(200, {'ok': true})
+      test('returns an authenticated github client', async () => {
+        api.get('/user')
+          .matchHeader('authorization', 'token test')
+          .reply(200, {hello: 'world'})
+
+        const client = await adapter.auth(1)
+        const res = await client.users.get()
+        expect(res.data).toEqual({hello: 'world'})
+      })
+
+      test('caches the token', async () => {
+        await adapter.auth(1)
+        // nock will fail here if there's a second request
+        await adapter.auth(1)
+      })
+    })
+
+    describe('without an installation id', () => {
+      test('returns an authenticated github client', async () => {
+        const github = await adapter.auth()
+
+        api.get('/app')
+          .matchHeader('authorization', /^Bearer/)
+          .reply(200, {name: 'test'})
+
+        const res = await github.apps.get()
+        expect(res.data).toEqual({name: 'test'})
       })
     })
   })

ca8de7ac883a1ebe8ea3ff287c86b62f2e59d0ca
tcbyrd <tcbyrd@github.com>
2018-03-19 01:49:02 -0400

    Reverting to clean state with the test

--- test/serverless.test.js
+++ test/serverless.test.js
@@ -4,8 +4,6 @@ const plugin = require('./fixtures/plugin/stub-plugin')
 
 describe('Serverless', function () {
   let probot
-  let provider
-  let lambdaInstance
 
   beforeEach(function () {
     probot = createProbot()
@@ -14,12 +12,12 @@ describe('Serverless', function () {
   describe('providers', function () {
     it('Responds with Lambda function signature when \'aws\' is specified', function () {
       lambdaInstance = (event, context, callback) => {}
-      expect(serverless(plugin, 'aws')).toBeInstanceOf(lambdaInstance)
+      expect(serverless(plugin, 'aws')).toEqual(lambdaInstance)
     })
 
     it('Responds with GCF function signature when \'gcf\' is specified', function () {
       gcfInstance = (request, response) => {}
-      expect(serverless(plugin, 'gcf')).toBeInstanceOf(gcfInstance)
+      expect(serverless(plugin, 'gcf')).toEqual(gcfInstance)
     })
 
     it('Throw an error when no provider is specified', function () {

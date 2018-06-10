794b8cce1fd3febf5e27437bda6ff9fa9a90986f
Brandon Keepers <bkeepers@github.com>
2018-06-09 21:33:32 -0500

    Update bottleneck usage

--- src/github/rate-limiting.ts
+++ src/github/rate-limiting.ts
@@ -2,7 +2,10 @@ const Bottleneck = require('bottleneck')
 
 export function addRateLimiting (octokit, limiter) {
   if (!limiter) {
-    limiter = new Bottleneck(1, 1000)
+    limiter = new Bottleneck({
+      maxConcurrent: 1,
+      minTime: 1000
+    })
   }
 
   const noop = () => Promise.resolve()

5fb469316552663108fa713653f2ce5f9db99fa8
Brandon Keepers <bkeepers@github.com>
2018-06-11 17:51:51 -0500

    Add options for Webhooks constructor

--- src/@types/@octokit/webhooks/index.d.ts
+++ src/@types/@octokit/webhooks/index.d.ts
@@ -1,10 +1,15 @@
 declare module '@octokit/webhooks' {
   import {Application} from 'express'
 
+  interface Options {
+    path: string,
+    secret: string
+  }
+
   declare class Webhooks {
     public middleware: Application
 
-    constructor()
+    constructor(Options)
   }
 
   export = Webhooks

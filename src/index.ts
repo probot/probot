565ad9a2460c45ef8fbc5eed23a4dddd1a61a874
Brandon Keepers <bkeepers@github.com>
2018-07-15 11:06:51 -0500

    Make most webhook payload properties optional for now
    Until we have a way to generate test fixtures, it's too much of a hassle 
    to require all these properties.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
+import { WebhookEvent } from '@octokit/webhooks'
 import Logger from 'bunyan'
 import cacheManager from 'cache-manager'
 import express from 'express'
 import { Application } from './application'
-import { Context, WebhookEvent } from './context'
+import { Context } from './context'
 import { createApp } from './github-app'
 import { logger } from './logger'
 import { resolve } from './resolver'
@@ -46,11 +47,8 @@ export class Probot {
     this.server = createServer({ webhook: this.webhook.middleware, logger })
 
     // Log all received webhooks
-    this.webhook.on('*', (event: any) => {
-      const webhookEvent = { ...event, event: event.name }
-      delete webhookEvent.name
-
-      return this.receive(webhookEvent)
+    this.webhook.on('*', (event: WebhookEvent) => {
+      return this.receive(event)
     })
 
     // Log all webhook errors

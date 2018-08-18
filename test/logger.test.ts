920bdc1b9fa4a4f5bd9115f32389063099f769a1
yawningphantom <siddharthsingh18196@gmail.com>
2018-08-18 11:05:04 +0530

    Convert logger test to Typescript

--- test/logger.test.ts
+++ test/logger.test.ts
@@ -2,22 +2,22 @@ import { logger } from '../src/logger'
 import { wrapLogger } from '../src/wrap-logger'
 
 describe('logger', () => {
-  let output: any
+  let output: string[]
   beforeEach(() => {
     output = []
 
     logger.addStream({
       level: 'trace',
-      stream: { write: (log: any) => output.push(log) },
+      stream: { write: (log: any) => output.push(log) } as any,
       type: 'raw'
-    } as any)
+    })
   })
 
   describe('child', () => {
     test('sets attributes', () => {
-      const child = wrapLogger(logger).child({ user: 'me' } as any)
+      const child = wrapLogger(logger).child({ id: '1234' })
       child.debug('attributes will get added to this')
-      expect(output[0]).toHaveProperty('user', 'me')
+      expect(output[0]).toHaveProperty('id', '1234')
     })
 
     test('allows setting the name', () => {

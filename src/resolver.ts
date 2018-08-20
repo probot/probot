4f2adc47f85f597caf1ad9cd125af468b5ae1f97
Karol Majewski <karol-majewski@users.noreply.github.com>
2018-08-20 22:58:06 +0200

    refactor: Extract the Resolver type

--- src/resolver.ts
+++ src/resolver.ts
@@ -4,11 +4,13 @@ export const resolve = (appFnId: string, opts?: ResolveOptions) => {
   opts = opts || defaultOptions
   // These are mostly to ease testing
   const basedir = opts.basedir || process.cwd()
-  const resolver = opts.resolver || require('resolve').sync
+  const resolver: Resolver = opts.resolver || require('resolve').sync
   return require(resolver(appFnId, { basedir }))
 }
 
+export type Resolver = (appFnId: string, opts: {basedir: string}) => string
+
 export interface ResolveOptions {
   basedir?: string
-  resolver?: (appFnId: string, opts: {basedir: string}) => string
+  resolver?: Resolver
 }

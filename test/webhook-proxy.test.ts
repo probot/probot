66b2da5a4f33fb983a19e8f137bafb8e03ad7c78
Mackie Underdown <macklinu@users.noreply.github.com>
2018-09-26 18:26:24 -0700

    chore(test): convert webhook-proxy test to TypeScript
    See #582

--- test/webhook-proxy.test.ts
+++ test/webhook-proxy.test.ts
@@ -1,13 +1,19 @@
-const express = require('express')
+import express from 'express'
+import { Server } from 'http'
+import nock from 'nock'
+import { logger } from '../src/logger'
+import { createWebhookProxy } from '../src/webhook-proxy'
+
+// tslint:disable-next-line:no-var-requires
 const sse = require('connect-sse')()
-const nock = require('nock')
-const {createWebhookProxy} = require('../src/webhook-proxy')
-const {logger} = require('../src/logger')
 
 const targetPort = 999999
 
 describe('webhook-proxy', () => {
-  let app, server, proxy, url, emit
+  let app: express.Application
+  let server: Server
+  let proxy: any
+  let emit: express.Send
 
   afterEach(() => {
     server && server.close()
@@ -18,14 +24,14 @@ describe('webhook-proxy', () => {
     beforeEach((done) => {
       app = express()
 
-      app.get('/events', sse, function (req, res) {
-        res.json({}, 'ready')
+      app.get('/events', sse, (req, res) => {
+        res.json({}, 'ready') // fixme: not sure how to convert this to TypeScript
         emit = res.json
       })
 
       server = app.listen(0, () => {
-        url = `http://127.0.0.1:${server.address().port}/events`
-        proxy = createWebhookProxy({url, port: targetPort, path: '/test', logger})
+        const url = `http://127.0.0.1:${server.address().port}/events`
+        proxy = createWebhookProxy({ url, port: targetPort, path: '/test', logger })
 
         // Wait for proxy to be ready
         proxy.addEventListener('ready', () => done())
@@ -37,11 +43,11 @@ describe('webhook-proxy', () => {
         done()
       })
 
-      const body = {action: 'foo'}
+      const body = { action: 'foo' }
 
       emit({
-        'x-github-event': 'test',
-        body
+        body,
+        'x-github-event': 'test'
       })
     })
   })
@@ -50,12 +56,13 @@ describe('webhook-proxy', () => {
     const url = 'http://bad.proxy/events'
     nock('http://bad.proxy').get('/events').reply(404)
 
-    const log = logger.child()
+    const options = {}
+    const log = logger.child(options)
     log.error = jest.fn()
 
-    proxy = createWebhookProxy({url, logger: log})
+    proxy = createWebhookProxy({ url, logger: log })
 
-    proxy.on('error', err => {
+    proxy.on('error', (err: any) => {
       expect(err.status).toBe(404)
       expect(log.error).toHaveBeenCalledWith(err)
       done()

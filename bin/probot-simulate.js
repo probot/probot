4dd8d95d461716d0dfd16132931db15b8af1d56d
Brandon Keepers <bkeepers@github.com>
2018-10-10 08:27:08 -0500

    Remove positional arguments

--- bin/probot-simulate.js
+++ bin/probot-simulate.js
@@ -9,19 +9,17 @@ const program = require('commander')
 const {findPrivateKey} = require('../lib/private-key')
 
 program
-  .usage('[options] [event-name] [path/to/payload.json] [path/to/app.js...]')
-  .option('-e, --event <event-name>', 'Event name', process.env.GITHUB_EVENT)
-  .option('-p, --event-path <event-path>', 'Event path', process.env.GITHUB_EVENT_PATH)
+  .usage('[options] [path/to/app.js...]')
+  .option('-e, --event <event-name>', 'Event name', process.env.GITHUB_EVENT_TYPE)
+  .option('-p, --payload-path <payload-path>', 'Path to the event payload', process.env.GITHUB_EVENT_PATH)
   .option('-t, --token <access-token>', 'Access token', process.env.GITHUB_TOKEN)
   .option('-a, --app <id>', 'ID of the GitHub App', process.env.APP_ID)
   .option('-P, --private-key <file>', 'Path to certificate of the GitHub App', findPrivateKey)
   .parse(process.argv)
 
-const eventName = program.args[0] || program.event
-const payloadPath = program.args[1] || program.eventPath
 const githubToken = program.token
 
-if (!eventName || !payloadPath || !githubToken) {
+if (!program.event || !program.payloadPath || !githubToken) {
   program.help()
 }
 
@@ -29,7 +27,7 @@ if (githubToken) {
   process.env.DISABLE_STATS = 'true'
 }
 
-const payload = require(path.join(process.cwd(), payloadPath))
+const payload = require(path.join(process.cwd(), program.payloadPath))
 
 const {createProbot} = require('../')
 
@@ -39,10 +37,10 @@ const probot = createProbot({
   githubToken: githubToken
 })
 
-probot.setup(program.args.slice(2))
+probot.setup(program.args)
 
-probot.logger.debug('Simulating event', eventName)
-probot.receive({name: eventName, payload, id: uuid.v4() })
+probot.logger.debug('Simulating event', program.event)
+probot.receive({name: program.event, payload, id: uuid.v4() })
   .catch(err => {
     // Process must exist non-zero to indicate that the action failed to run
     // TODO: this is not working yet

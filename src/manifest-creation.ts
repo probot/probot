6dc8dccdf83a8da98ca21770e070de5ab466eb2f
Gregor <gregor@martynus.net>
2019-01-08 00:24:35 -0800

    refactor: github.request(route, options) is so pretty

--- src/manifest-creation.ts
+++ src/manifest-creation.ts
@@ -58,10 +58,9 @@ export class ManifestCreation {
 
   public async createAppFromCode (code: any) {
     const github = GitHubAPI()
-    const response = await github.request({
-      headers: { accept: 'application/vnd.github.fury-preview+json' },
-      method: 'POST',
-      url: `/app-manifests/${code}/conversions`
+    const response = await github.request('POST /app-manifests/:code/conversions', {
+      code,
+      headers: { accept: 'application/vnd.github.fury-preview+json' }
     })
 
     const { id, webhook_secret, pem } = response.data

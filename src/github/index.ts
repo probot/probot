6f04523694fd4d820c1e4ed7cb82982bf222c10a
Adam Netočný <adam.netocny@deutsche-boerse.com>
2019-01-31 11:08:09 +0100

    Add default Octokit option if no options are provided

--- src/github/index.ts
+++ src/github/index.ts
@@ -19,7 +19,7 @@ export const ProbotOctokit = Octokit
  * browser.
  * @see {@link https://github.com/octokit/rest.js}
  */
-export function GitHubAPI (options: Options = {} as any) {
+export function GitHubAPI (options: Options = { Octokit: ProbotOctokit } as any) {
   const octokit = new options.Octokit(Object.assign(options, {
     throttle: Object.assign({
       onAbuseLimit: (retryAfter: number) => options.logger.warn(`Abuse limit hit, retrying in ${retryAfter} seconds`),

07d93640d010103a897ed58947809c0aeacc75cd
wolfy1339 <4595477+wolfy1339@users.noreply.github.com>
2019-02-08 14:51:39 -0500

    Fix the example test

--- test/fixtures/example.ts
+++ test/fixtures/example.ts
@@ -3,7 +3,7 @@ import { Application } from '../../src/application'
 export = (app: Application) => {
   app.log('loaded app')
 
-  app.on('issue_comment.created', context => {
+  app.on('issue_comment.created', async context => {
     context.log('Comment created')
   })
 }

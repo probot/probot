507cb3d09cdb4375d34d0db4ef186f52ff645e22
Thomas Hughes <iamhughes@github.com>
2019-02-08 21:16:46 -0600

    Update spelling for documentation

--- src/context.ts
+++ src/context.ts
@@ -111,7 +111,7 @@ export class Context implements WebhookEvent {
    * comment: Check the specs on the rotary girder.
    * ```
    *
-   * You app can read that file from the target repository:
+   * Your app can read that file from the target repository:
    *
    * ```js
    * // Load config from .github/config.yml in the repository

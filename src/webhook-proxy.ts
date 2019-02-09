96c4b38d0e829b58c97d7c74e35da2fcc7bd0e41
wolfy1339 <webmaster@wolfy1339.com>
2019-02-09 01:48:51 -0500

    Fix some errors
    - Convert const/require import to a typescript CommonJs require
    - Import EventSource to silence the typescript compiler

--- src/webhook-proxy.ts
+++ src/webhook-proxy.ts
@@ -1,10 +1,11 @@
 import Logger from 'bunyan'
+// @ts-ignore
+// tslint:disable-next-line:no-implicit-dependencies
+import EventSource from 'eventsource' // This is needed for now to resolve error TS4023, until smee-client gets it's own types or get's rewritten in TypeScript.
+import SmeeClient = require('smee-client')
 
 export const createWebhookProxy = (opts: WebhookProxyOptions) => {
   try {
-    // tslint:disable-next-line
-    const SmeeClient = require('smee-client')
-
     const smee = new SmeeClient({
       logger: opts.logger,
       source: opts.url,

8c5495fc80c9ca82c5b4b6ce19110fc2bd18942c
Alberto Gimeno <gimenete@users.noreply.github.com>
2019-02-20 17:58:10 +0100

    feat: standalone run command (#751)

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,11 @@
 const Bottleneck = require('bottleneck')
+const { Probot, createProbot } = require('../src')
 const nock = require('nock')
 const Octokit = require('@octokit/rest')
 const request = require('supertest')
 
-const { createProbot } = require('../src')
 const helper = require('./apps/helper')
+const path = require('path')
 
 describe('Probot', () => {
   let probot
@@ -24,6 +25,51 @@ describe('Probot', () => {
     createProbot({ githubToken: 'faketoken' })
     // probot with id/cert
     createProbot({ id: 1234, cert: 'xxxx' })
+  })
+
+  describe('run', () => {
+
+    let env
+
+    beforeAll(() => {
+      env = { ...process.env }
+      process.env.APP_ID = '1'
+      process.env.PRIVATE_KEY_PATH = path.join(__dirname, 'test-private-key.pem')
+      process.env.WEBHOOK_PROXY_URL = 'https://smee.io/EfHXC9BFfGAxbM6J'
+      process.env.DISABLE_STATS = 'true'
+    })
+
+    afterAll(() => {
+      process.env = env
+    })
+
+    it('runs with a function as argument', async () => {
+      process.env.PORT = '3003'
+      let initialized = false
+      const probot = await Probot.run((app) => {
+        initialized = true
+      })
+      expect(probot.options).toMatchSnapshot()
+      expect(initialized).toBeTruthy()
+      probot.httpServer.close()
+    })
+
+    it('runs with an array of strings', async () => {
+      const probot = await Probot.run(['run', 'file.js'])
+      expect(probot.options).toMatchSnapshot()
+      probot.httpServer.close()
+    })
+
+    it('runs without config and loads the setup app', async () => {
+      let initialized = false
+      delete process.env.PRIVATE_KEY_PATH
+      const probot = await Probot.run((app) => {
+        initialized = true
+      })
+      expect(probot.options).toMatchSnapshot()
+      expect(initialized).toBeFalsy()
+      probot.httpServer.close()
+    })
   })
 
   describe('webhook delivery', () => {

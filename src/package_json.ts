bf2679972a2c2e46ffc62ebc9624d9716658907b
wolfy1339 <webmaster@wolfy1339.com>
2019-03-09 04:34:28 -0500

    Fix some linting errors

--- src/package_json.ts
+++ src/package_json.ts
@@ -1,98 +1,98 @@
-export interface PackageJSON extends Object {
-  readonly name: string;
-  readonly version?: string;
-  readonly description?: string;
-  readonly keywords?: string[];
-  readonly homepage?: string;
-  readonly bugs?: string | IBugs;
-  readonly license?: string;
-  readonly author?: string | IAuthor;
-  readonly contributors?: string[] | IAuthor[];
-  readonly files?: string[];
-  readonly main?: string;
-  readonly bin?: string | IBinMap;
-  readonly man?: string | string[];
-  readonly directories?: IDirectories;
-  readonly repository?: string | IRepository;
-  readonly scripts?: IScriptsMap;
-  readonly config?: IConfig;
-  readonly dependencies?: IDependencyMap;
-  readonly devDependencies?: IDependencyMap;
-  readonly peerDependencies?: IDependencyMap;
-  readonly optionalDependencies?: IDependencyMap;
-  readonly bundledDependencies?: string[];
-  readonly engines?: IEngines;
-  readonly os?: string[];
-  readonly cpu?: string[];
-  readonly preferGlobal?: boolean;
-  readonly private?: boolean;
-  readonly publishConfig?: IPublishConfig;
+export interface PackageJSON {
+  readonly name: string
+  readonly version?: string
+  readonly description?: string
+  readonly keywords?: string[]
+  readonly homepage?: string
+  readonly bugs?: string | Bugs
+  readonly license?: string
+  readonly author?: string | Author
+  readonly contributors?: string[] | Author[]
+  readonly files?: string[]
+  readonly main?: string
+  readonly bin?: string | BinMap
+  readonly man?: string | string[]
+  readonly directories?: Directories
+  readonly repository?: string | Repository
+  readonly scripts?: ScriptsMap
+  readonly config?: Config
+  readonly dependencies?: DependencyMap
+  readonly devDependencies?: DependencyMap
+  readonly peerDependencies?: DependencyMap
+  readonly optionalDependencies?: DependencyMap
+  readonly bundledDependencies?: string[]
+  readonly engines?: Engines
+  readonly os?: string[]
+  readonly cpu?: string[]
+  readonly preferGlobal?: boolean
+  readonly private?: boolean
+  readonly publishConfig?: PublishConfig
 }
 
 /**
  * An author or contributor
  */
-interface IAuthor {
-  name: string;
-  email?: string;
-  homepage?: string;
+interface Author {
+  name: string
+  email?: string
+  homepage?: string
 }
 
 /**
  * A map of exposed bin commands
  */
-interface IBinMap {
-  [commandName: string]: string;
+interface BinMap {
+  [commandName: string]: string
 }
 
 /**
  * A bugs link
  */
-interface IBugs {
-  email: string;
-  url: string;
+interface Bugs {
+  email: string
+  url: string
 }
 
-interface IConfig {
-  name?: string;
-  config?: Object;
+interface Config {
+  name?: string
+  config?: Object
 }
 
 /**
  * A map of dependencies
  */
-interface IDependencyMap {
-  [dependencyName: string]: string;
+interface DependencyMap {
+  [dependencyName: string]: string
 }
 
 /**
  * CommonJS package structure
  */
-interface IDirectories {
-  lib?: string;
-  bin?: string;
-  man?: string;
-  doc?: string;
-  example?: string;
+interface Directories {
+  lib?: string
+  bin?: string
+  man?: string
+  doc?: string
+  example?: string
 }
 
-interface IEngines {
-  node?: string;
-  npm?: string;
+interface Engines {
+  node?: string
+  npm?: string
 }
 
-interface IPublishConfig {
-  registry?: string;
+interface PublishConfig {
+  registry?: string
 }
 
 /**
  * A project repository
  */
-interface IRepository {
-  type: string;
-  url: string;
+interface Repository {
+  type: string
+  url: string
 }
 
-interface IScriptsMap {
-  [scriptName: string]: string;
+interface ScriptsMap {
+  [scriptName: string]: string
 }

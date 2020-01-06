804f62be0e1b454f1c1d58375cd27967a174843a
jamesgeorge007 <jamesgeorge998001@gmail.com>
2020-01-06 13:33:21 +0530

    feat: configure update-notifier

--- bin/probot.js
+++ bin/probot.js
@@ -3,10 +3,15 @@
 const semver = require('semver')
 const program = require('commander')
 const pkg = require('../package')
+const updateNotifier = require('update-notifier')
 
 if (!semver.satisfies(process.version, pkg.engines.node)) {
   console.log(`Node.js version ${pkg.engines.node} is required. You have ${process.version}.`)
   process.exit(1)
+}
+
+if (pkg.version !== '0.0.0-development') {
+  updateNotifier({pkg}).notify()
 }
 
 program
@@ -16,9 +21,9 @@ program
   .command('receive', 'Receive a single event and payload')
   .on('command:*', (cmd) => {
     if (!program.commands.find(c => c._name == cmd[0])) {
-      console.error(`Invalid command: ${program.args.join(' ')}\n`);
+      console.error(`Invalid command: ${program.args.join(' ')}\n`)
       program.outputHelp()
-      process.exit(1);
+      process.exit(1)
     }
   })
 

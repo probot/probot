bd46959e62ed87432af1c55ed1d0ade6f4e51b18
Gregor Martynus <gregor@martynus.net>
2020-08-14 12:35:17 -0700

    fix: remove removal messages about `LOG_FORMAT`, `LOG_LEVEL_IN_STRING`, and `SENTRY_DSN`

--- src/handle-deprecated-environment-variables.ts
+++ src/handle-deprecated-environment-variables.ts
@@ -13,25 +13,4 @@ export function handleDeprecatedEnvironmentVariables() {
     // tslint:disable:no-console
     log.warn('[probot] "IGNORED_ACCOUNTS" has been removed in v10');
   }
-
-  if ("LOG_FORMAT" in process.env) {
-    // tslint:disable:no-console
-    log.warn(
-      '[probot] "LOG_FORMAT" has been removed in v10. We now use https://getpino.io/ for logging. prettyPrint is enabled unless NODE_ENV is set to anything other than "development"'
-    );
-  }
-
-  if ("LOG_LEVEL_IN_STRING" in process.env) {
-    // tslint:disable:no-console
-    log.warn(
-      '[probot] "LOG_LEVEL_IN_STRING" has been removed in v10. Pipe the output to pino-text-level-transport instead. Example: `NODE_ENV=production probot run index.js | pino-text-level-transport`'
-    );
-  }
-
-  if ("SENTRY_DSN" in process.env) {
-    // tslint:disable:no-console
-    log.warn(
-      '[probot] "SENTRY_DSN" has been removed in v10. Use pino-sentry instead and make sure to set NODE_ENV. Example: `NODE_ENV=production probot run index.js | pino-sentry`'
-    );
-  }
 }

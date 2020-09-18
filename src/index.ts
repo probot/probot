dc4cd5319802de6eebf4d40675fcb8100fe9734a
Alex <shaftoe@users.noreply.github.com>
2020-09-18 22:45:52 +0300

    fix: remove redundant v from Node.js version log string (#1352)

--- src/index.ts
+++ src/index.ts
@@ -315,7 +315,7 @@ export class Probot {
 
   public start() {
     this.log.info(
-      `Running Probot v${this.version} (Node.js: v${process.version})`
+      `Running Probot v${this.version} (Node.js: ${process.version})`
     );
     const port = this.options.port || 3000;
     const { host } = this.options;

eab6cad6d1d04dff67f7bb9ea11cf42e9122b807
Ankeet Maini <ankeet.maini@gmail.com>
2020-10-08 20:01:18 +0530

    Upgrade octokit/webhooks

--- src/octokit/get-webhooks.ts
+++ src/octokit/get-webhooks.ts
@@ -1,16 +1,17 @@
-import { Webhooks } from "@octokit/webhooks";
+import { WebhookEvent, Webhooks } from "@octokit/webhooks";
 
 import { State } from "../types";
 import { getErrorHandler } from "../helpers/get-error-handler";
 import { webhookTransform } from "./octokit-webhooks-transform";
 
+import { Context } from "../context";
+
 export function getWebhooks(state: State) {
-  const webhooks = new Webhooks({
+  const webhooks = new Webhooks<WebhookEvent, Pick<Context, "github" | "log">>({
     path: state.webhooks.path,
     secret: state.webhooks.secret,
     transform: webhookTransform.bind(null, state),
   });
   webhooks.on("error", getErrorHandler(state.log));
-
   return webhooks;
 }

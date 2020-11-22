5c2d3c39d5d875656b6936b63386aca06859f0ac
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2020-11-22 13:00:23 -0800

    test: rewrite server tests to test new `Server` class

--- test/server.test.ts
+++ test/server.test.ts
@@ -1,18 +1,32 @@
-import { Application, NextFunction, Request, Response } from "express";
-import request from "supertest";
-import { getLog } from "../src/helpers/get-log";
-import { createServer } from "../src/server/create-server";
+import Stream from "stream";
 
-describe("server", () => {
-  let server: Application;
+import { NextFunction, Request, Response } from "express";
+import request from "supertest";
+import pino from "pino";
+
+import { Server } from "../src";
+
+describe("Server", () => {
+  let server: Server;
   let webhook: any;
 
+  let output: any[];
+  const streamLogsToOutput = new Stream.Writable({ objectMode: true });
+  streamLogsToOutput._write = (object, encoding, done) => {
+    output.push(JSON.parse(object));
+    done();
+  };
+
   beforeEach(() => {
+    output = [];
     webhook = jest.fn((req, res, next) => next());
-    server = createServer({ webhook, logger: getLog({ level: "fatal" }) });
+    server = new Server({
+      log: pino(streamLogsToOutput),
+    });
+    server.app.use(webhook);
 
     // Error handler to avoid printing logs
-    server.use(
+    server.app.use(
       (error: Error, req: Request, res: Response, next: NextFunction) => {
         res.status(500).send(error.message);
       }
@@ -20,24 +34,30 @@ describe("server", () => {
   });
 
   describe("GET /ping", () => {
-    it("returns a 200 response", () => {
-      return request(server).get("/ping").expect(200, "PONG");
+    it("returns a 200 response", async () => {
+      await request(server.app).get("/ping").expect(200, "PONG");
+      expect(output.length).toEqual(1);
+      expect(output[0].msg).toContain("GET /ping 200 -");
     });
   });
 
   describe("webhook handler", () => {
-    it("should 500 on a webhook error", () => {
+    it("should 500 on a webhook error", async () => {
       webhook.mockImplementation(
         (req: Request, res: Response, callback: NextFunction) =>
           callback(new Error("webhook error"))
       );
-      return request(server).post("/").expect(500);
+      await request(server.app).post("/").expect(500);
+      expect(output.length).toEqual(1);
+      expect(output[0].msg).toContain("POST / 500 -");
     });
   });
 
   describe("with an unknown url", () => {
-    it("responds with 404", () => {
-      return request(server).get("/lolnotfound").expect(404);
+    it("responds with 404", async () => {
+      await request(server.app).get("/notfound").expect(404);
+      expect(output.length).toEqual(1);
+      expect(output[0].msg).toContain("GET /notfound 404 -");
     });
   });
 });

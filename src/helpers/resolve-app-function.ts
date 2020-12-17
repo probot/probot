335684509e0aab140014c96a35e03b46ff6f64dc
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2020-12-17 15:03:59 -0800

    fix: import app functions transpiled from TypeScript (#1448)

--- src/helpers/resolve-app-function.ts
+++ src/helpers/resolve-app-function.ts
@@ -7,7 +7,20 @@ export const resolveAppFunction = (appFnId: string, opts?: ResolveOptions) => {
   // These are mostly to ease testing
   const basedir = opts.basedir || process.cwd();
   const resolver: Resolver = opts.resolver || sync;
-  return require(resolver(appFnId, { basedir }));
+  const appFnPath = resolver(appFnId, { basedir });
+  const mod = require(appFnPath);
+
+  if (typeof mod === "function") {
+    return mod;
+  }
+
+  // handle ES Module export transpiled to JS
+  // https://github.com/probot/probot/issues/1447
+  if (mod.__esModule && typeof mod.default === "function") {
+    return mod.default;
+  }
+
+  throw new Error(`[probot] now app function found at ${appFnPath}`);
 };
 
 export type Resolver = (appFnId: string, opts: { basedir: string }) => string;

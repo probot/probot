7863b7d6524580bbfa7b0442c7d2473863e3bab8
Björn Brauer <zaubernerd@zaubernerd.de>
2021-01-15 00:38:32 +0100

    fix: correctly import (transpiled) app function for run and receive (#1457) thanks @ZauberNerd

--- src/helpers/resolve-app-function.ts
+++ src/helpers/resolve-app-function.ts
@@ -2,25 +2,18 @@ import { sync } from "resolve";
 
 const defaultOptions: ResolveOptions = {};
 
-export const resolveAppFunction = (appFnId: string, opts?: ResolveOptions) => {
+export const resolveAppFunction = async (
+  appFnId: string,
+  opts?: ResolveOptions
+) => {
   opts = opts || defaultOptions;
   // These are mostly to ease testing
   const basedir = opts.basedir || process.cwd();
   const resolver: Resolver = opts.resolver || sync;
   const appFnPath = resolver(appFnId, { basedir });
-  const mod = require(appFnPath);
-
-  if (typeof mod === "function") {
-    return mod;
-  }
-
-  // handle ES Module export transpiled to JS
-  // https://github.com/probot/probot/issues/1447
-  if (mod.__esModule && typeof mod.default === "function") {
-    return mod.default;
-  }
-
-  throw new Error(`[probot] now app function found at ${appFnPath}`);
+  const mod = await import(appFnPath);
+  // Note: This needs "esModuleInterop" to be set to "true" in "tsconfig.json"
+  return mod.default;
 };
 
 export type Resolver = (appFnId: string, opts: { basedir: string }) => string;

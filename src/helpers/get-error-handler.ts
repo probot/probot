be0a02040cd1aa43d8358436fda12b86b02aa435
Chris <github@mms2k.de>
2021-01-20 16:45:16 +0100

    docs: Enhance error message on wrong pem error

--- src/helpers/get-error-handler.ts
+++ src/helpers/get-error-handler.ts
@@ -23,7 +23,7 @@ export function getErrorHandler(log: Logger) {
       if (errMessage.includes("pem") || errMessage.includes("json web token")) {
         log.error(
           error,
-          "Your private key (usually a .pem file) is not correct. Go to https://github.com/settings/apps/YOUR_APP and generate a new PEM file. If you're deploying to Now, visit https://probot.github.io/docs/deployment/#now."
+          "Your private key (a .pem file or PRIVATE_KEY environment variable) or APP_ID is incorrect. Go to https://github.com/settings/apps/YOUR_APP, verify that APP_ID is set correctly, and generate a new PEM file if necessary."
         );
         continue;
       }

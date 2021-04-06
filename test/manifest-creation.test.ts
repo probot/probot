d70c4af7481c01c240558f2ff7e7a62bd2606157
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-04-05 19:11:56 -0700

    build(package): fix description

--- test/manifest-creation.test.ts
+++ test/manifest-creation.test.ts
@@ -126,7 +126,7 @@ describe("ManifestCreation", () => {
     test("creates an app from a code", () => {
       // checks that getManifest returns a JSON.stringified manifest
       expect(setup.getManifest(pkg, "localhost://3000")).toEqual(
-        '{"description":"🤖 A framework for building GitHub Apps to automate and improve your workflow","hook_attributes":{"url":"localhost://3000/"},"name":"probot","public":true,"redirect_url":"localhost://3000/probot/setup","url":"https://probot.github.io","version":"v1"}'
+        '{"description":"A framework for building GitHub Apps to automate and improve your workflow","hook_attributes":{"url":"localhost://3000/"},"name":"probot","public":true,"redirect_url":"localhost://3000/probot/setup","url":"https://probot.github.io","version":"v1"}'
       );
     });
 

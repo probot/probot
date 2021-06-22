26574c9f3a17325f0be02cf7b09b1a7c02bd25e1
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-06-22 15:00:57 -0700

    docs: add comment about `new Webhooks<Context>({...})`

--- src/octokit/get-webhooks.ts
+++ src/octokit/get-webhooks.ts
@@ -7,6 +7,9 @@ import { webhookTransform } from "./octokit-webhooks-transform";
 // import { Context } from "../context";
 
 export function getWebhooks(state: State) {
+  // TODO: This should be webhooks = new Webhooks<Context>({...}) but fails with
+  //       > The context of the event that was triggered, including the payload and
+  //         helpers for extracting information can be passed to GitHub API calls
   const webhooks = new Webhooks({
     secret: state.webhooks.secret!,
     transform: webhookTransform.bind(null, state),

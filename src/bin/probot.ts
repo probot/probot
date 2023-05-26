b295eb632ed7d891a944d8f8e96f81b627ed0674
Gregor Adams <greg@pixelass.com>
2023-05-26 21:04:56 +0200

    feat: add node version check back
    make previously removed version check esm compatible

--- src/bin/probot.ts
+++ src/bin/probot.ts
@@ -1,10 +1,30 @@
 import program from "commander";
-
+import fs from "fs";
+import { fileURLToPath } from "node:url";
+import path from "path";
+import semver from "semver";
 import dotenv from "dotenv";
 
 dotenv.config();
 
+export const __filename = fileURLToPath(import.meta.url);
+export const __dirname = path.dirname(__filename);
+
+const pkg = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, "../../package.json"), {
+    encoding: "utf-8",
+  })
+);
+
+if (!semver.satisfies(process.version, pkg.engines.node)) {
+  console.log(
+    `Node.js version ${pkg.engines.node} is required. You have ${process.version}.`
+  );
+  process.exit(1);
+}
+
 program
+  .version(pkg.version)
   .usage("<command> [options]")
   .command("run", "run the bot")
   .command("receive", "Receive a single event and payload")

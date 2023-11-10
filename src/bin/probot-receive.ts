bd7ae1eacc8943e5e01034ccb04e579a2a62c249
uzlopak <aras.abbasi@googlemail.com>
2023-11-10 17:55:51 +0100

    upgrade commander

--- src/bin/probot-receive.ts
+++ src/bin/probot-receive.ts
@@ -6,7 +6,7 @@ require("dotenv").config();
 
 import path from "path";
 import { randomUUID as uuidv4 } from "crypto";
-import program from "commander";
+import { program } from "commander";
 import { getPrivateKey } from "@probot/get-private-key";
 import { getLog } from "../helpers/get-log";
 
@@ -73,34 +73,34 @@ async function main() {
     )
     .parse(process.argv);
 
-  const githubToken = program.token;
+  const githubToken = program.opts().token;
 
-  if (!program.event || !program.payloadPath) {
+  if (!program.opts().event || !program.opts().payloadPath) {
     program.help();
   }
 
   const privateKey = getPrivateKey();
-  if (!githubToken && (!program.app || !privateKey)) {
+  if (!githubToken && (!program.opts().app || !privateKey)) {
     console.warn(
       "No token specified and no certificate found, which means you will not be able to do authenticated requests to GitHub",
     );
   }
 
-  const payload = require(path.resolve(program.payloadPath));
+  const payload = require(path.resolve(program.opts().payloadPath));
   const log = getLog({
-    level: program.logLevel,
-    logFormat: program.logFormat,
-    logLevelInString: program.logLevelInString,
-    logMessageKey: program.logMessageKey,
-    sentryDsn: program.sentryDsn,
+    level: program.opts().logLevel,
+    logFormat: program.opts().logFormat,
+    logLevelInString: program.opts().logLevelInString,
+    logMessageKey: program.opts().logMessageKey,
+    sentryDsn: program.opts().sentryDsn,
   });
 
   const probot = new Probot({
-    appId: program.app,
+    appId: program.opts().app,
     privateKey: String(privateKey),
     githubToken: githubToken,
     log,
-    baseUrl: program.baseUrl,
+    baseUrl: program.opts().baseUrl,
   });
 
   const expressApp = express();
@@ -117,11 +117,13 @@ async function main() {
   );
   await probot.load(appFn, options);
 
-  probot.log.debug("Receiving event", program.event);
-  probot.receive({ name: program.event, payload, id: uuidv4() }).catch(() => {
-    // Process must exist non-zero to indicate that the action failed to run
-    process.exit(1);
-  });
+  probot.log.debug("Receiving event", program.opts().event);
+  probot
+    .receive({ name: program.opts().event, payload, id: uuidv4() })
+    .catch(() => {
+      // Process must exist non-zero to indicate that the action failed to run
+      process.exit(1);
+    });
 }
 
 main().catch((error) => {

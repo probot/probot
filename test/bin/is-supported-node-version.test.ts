0251f68697fc3a4bb1b8a32edf2eb8efb4828542
Aras Abbasi <aras.abbasi@googlemail.com>
2023-11-12 18:50:03 +0100

    fix(deps): -semver (#1893)
    chore: remove semver as dependency

---
+++ test/bin/is-supported-node-version.test.ts
@@ -0,0 +1,21 @@
+import { isSupportedNodeVersion } from "../../src/helpers/is-supported-node-version";
+import { engines } from "../../package.json";
+
+describe("isSupportedNodeVersion", () => {
+  it(`engines value is set to ">=18"`, () => {
+    expect(engines.node).toBe(">=18");
+  });
+
+  it("returns true if node is bigger or equal v18", () => {
+    expect(isSupportedNodeVersion("18.0.0")).toBe(true);
+    expect(isSupportedNodeVersion("19.0.0")).toBe(true);
+    expect(isSupportedNodeVersion("20.0.0")).toBe(true);
+    expect(isSupportedNodeVersion("21.0.0")).toBe(true);
+  });
+
+  it("returns false if node is smaller than v18", () => {
+    expect(isSupportedNodeVersion("17.0.0")).toBe(false);
+    expect(isSupportedNodeVersion("17.9.0")).toBe(false);
+    expect(isSupportedNodeVersion("17.9.9")).toBe(false);
+  });
+});

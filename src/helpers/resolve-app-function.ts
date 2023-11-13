00d023462acca040c1bf35b5b41afae6bde7fbef
Aaron Dewes <aaron.dewes@protonmail.com>
2023-11-13 11:02:46 +0100

    Fix for windows

--- src/helpers/resolve-app-function.ts
+++ src/helpers/resolve-app-function.ts
@@ -11,7 +11,13 @@ export const resolveAppFunction = async (
   const basedir = opts.basedir || process.cwd();
   const resolver: Resolver = opts.resolver || sync;
   const appFnPath = resolver(appFnId, { basedir });
-  const { default: mod } = await import(appFnPath);
+  // On windows, an absolute path may start with a drive letter, e.g. C:/path/to/file.js
+  // This can be interpreted as a protocol, so ensure it's prefixed with file://
+  const appFnPathWithFileProtocol = appFnPath.replace(
+    /^([a-zA-Z]:)/,
+    "file://$1",
+  );
+  const { default: mod } = await import(appFnPathWithFileProtocol);
   // mod.default gets exported by transpiled TypeScript code
   return mod.__esModule && mod.default ? mod.default : mod;
 };

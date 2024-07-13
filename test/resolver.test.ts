9de158c071ab7f6869f9f488af90aec829e534ad
Aras Abbasi <aras.abbasi@googlemail.com>
2024-07-13 11:18:37 +0200

    fix: remove resolve dep (#2051)

--- test/resolver.test.ts
+++ test/resolver.test.ts
@@ -10,15 +10,19 @@ const basedir = process.cwd();
 describe("resolver", () => {
   it("loads the module at the resolved path", async () => {
     const stubResolver = vi.fn().mockReturnValue(stubAppFnPath);
-    const module = await resolveAppFunction("foo", { resolver: stubResolver });
+    const module = await resolveAppFunction("foo", {
+      resolver: stubResolver as unknown as RequireResolve,
+    });
     expect(module).toBeInstanceOf(Function);
-    expect(stubResolver).toHaveBeenCalledWith("foo", { basedir });
+    expect(stubResolver).toHaveBeenCalledWith("foo", { paths: [basedir] });
   });
 
   it("loads module transpiled from TypeScript (https://github.com/probot/probot/issues/1447)", async () => {
     const stubResolver = vi.fn().mockReturnValue(stubTranspiledAppFnPath);
-    const module = await resolveAppFunction("foo", { resolver: stubResolver });
+    const module = await resolveAppFunction("foo", {
+      resolver: stubResolver as unknown as RequireResolve,
+    });
     expect(module).toBeInstanceOf(Function);
-    expect(stubResolver).toHaveBeenCalledWith("foo", { basedir });
+    expect(stubResolver).toHaveBeenCalledWith("foo", { paths: [basedir] });
   });
 });

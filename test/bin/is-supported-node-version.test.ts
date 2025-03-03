d1160cac5320a560ea8c00c0bbfe8b79f38c5cae
wolfy1339 <webmaster@wolfy1339.com>
2025-03-03 12:27:05 -0500

    fix: update is-supported-node-version

--- test/bin/is-supported-node-version.test.ts
+++ test/bin/is-supported-node-version.test.ts
@@ -5,20 +5,22 @@ import { loadPackageJson } from "../../src/helpers/load-package-json.js";
 
 describe("isSupportedNodeVersion", () => {
   const { engines } = loadPackageJson();
-  it(`engines value is set to ">=18"`, () => {
-    expect(engines!.node).toBe(">=18");
+  it(`engines value is set to "^20.17 || >= 22"`, () => {
+    expect(engines!.node).toBe("^20.17 || >= 22");
   });
 
-  it("returns true if node is bigger or equal v18", () => {
-    expect(isSupportedNodeVersion("18.0.0")).toBe(true);
-    expect(isSupportedNodeVersion("19.0.0")).toBe(true);
-    expect(isSupportedNodeVersion("20.0.0")).toBe(true);
-    expect(isSupportedNodeVersion("21.0.0")).toBe(true);
+  it("returns true if node is bigger or equal v20.17 or v22", () => {
+    expect(isSupportedNodeVersion("20.17.0")).toBe(true);
+    expect(isSupportedNodeVersion("22.0.0")).toBe(true);
+    expect(isSupportedNodeVersion("23.0.0")).toBe(true);
   });
 
-  it("returns false if node is smaller than v18", () => {
+  it("returns false if node is smaller than v20.17", () => {
     expect(isSupportedNodeVersion("17.0.0")).toBe(false);
     expect(isSupportedNodeVersion("17.9.0")).toBe(false);
     expect(isSupportedNodeVersion("17.9.9")).toBe(false);
+    expect(isSupportedNodeVersion("18.0.0")).toBe(false);
+    expect(isSupportedNodeVersion("20.16.9")).toBe(false);
+    expect(isSupportedNodeVersion("21.0.0")).toBe(false);
   });
 });
